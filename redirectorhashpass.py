322fdaae7276c5b96a639acb4f7e48ca89e18140
DykemaBill <git@dykema.org>
2020-07-18 14:27:42 -0500

    Password hash util updated

--- redirectorhashpass.py
+++ redirectorhashpass.py
@@ -11,25 +11,22 @@ def main(mypassword):
         pass_salt = bcrypt.gensalt()
         # Create hash of the password using the salt
         pass_hashed = bcrypt.hashpw(pass_encoded, pass_salt)
-        # Decode salt and password hashed to make it easier to store
-        pass_salt_decoded = pass_salt.decode("utf-8")
+        # Decode password hashed to make it easier to store
         pass_hashed_decoded = pass_hashed.decode("utf-8")
-        # Combine them together
-        pass_to_store = pass_salt_decoded + "-" + pass_hashed_decoded
-        
+
         print ("Here is your password in clear text: " + mypassword)
-        print ("Here is your salt and hashed password: " + str(pass_to_store))
+        print ("Here is your salt and hashed password: " + str(pass_hashed_decoded))
 
         # Hash test
         # Get the new password
         test_password = mypassword
         # Encode the new password
         test_password_encoded = b64encode(test_password.encode("utf-8"))
-        # Get the salt previously used
-        test_pass_salt_decoded = pass_to_store.split('-')[0]
-        # Convert the previously used salt from its decoded state to a byte
+        # Get the salt, first 29 characters
+        test_pass_salt_decoded = pass_hashed_decoded[:29]
+        # Convert the salt to a byte
         test_pass_salt = test_pass_salt_decoded.encode("utf-8")
-        # Create hash of new password using previously used salt
+        # Create hash of new password using salt
         test_pass_hashed = bcrypt.hashpw(test_password_encoded, test_pass_salt)
         # Decode the hash since that is how it would be stored
         test_pass_hashed_decoded = test_pass_hashed.decode("utf-8")
