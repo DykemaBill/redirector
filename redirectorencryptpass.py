ef22b27dc2c21bde5d4a289681a612c643274171
Bill Dykema <bill@Bills-MacBook-Air.local>
2020-07-30 09:44:59 -0500

    Password encryption util clean-up

--- redirectorencryptpass.py
+++ redirectorencryptpass.py
@@ -1,39 +1,52 @@
 import sys
-from base64 import b64encode, b64decode
 from cryptography.fernet import Fernet
 
-# Encyption
-def passencrypt(mypassword):
-    if mypassword:
-        # Generate key
-        key = Fernet.generate_key()
-        # Decoded encryption key to be stored
-        keyDecoded = key.decode("utf-8")
-        print ("Here is your decoded key to be stored: " + str(keyDecoded))
-        # Set key
-        encryptKey = Fernet(key)
-        # Encrypt password from byte form with str.encode
-        encryptPass = encryptKey.encrypt(str.encode(mypassword))
-        # Decode encrypted password to string form to be stored
-        encryptPassDecoded = encryptPass.decode("utf-8")
-        print ("Here is your encrypted password be stored: " + str(encryptPassDecoded))
+# Generate encryption key
+def passkey():
+    # Generate key
+    key = Fernet.generate_key()
+    # Decoded encryption key to be stored
+    keyDecoded = key.decode("utf-8")
+    return keyDecoded
 
-        # Take key from stored form and convert it back to byte form
-        keyRecovered = keyDecoded.encode("utf-8")
-        print ("Here is your original key: " + str(keyRecovered))
-        encryptKeyRecovered = Fernet(keyRecovered)
-        # Using recovered key unencrypt stored password
-        encryptPassEncode = encryptPassDecoded.encode("utf-8")
-        print ("Here is your password before unencrypting it: " + str(encryptPassEncode))
-        originalPassByte = encryptKeyRecovered.decrypt(encryptPassEncode)
-        originalPass = originalPassByte.decode("utf-8")
-        print ("Here is your password after unencrypting: " + str(originalPass))
-    else:
-        print ("You did not give me a password to encrypt!")
+# Encrypt password using key
+def passencrypt(mykey, mypassword):
+    # Take key from stored form and convert it back to byte form
+    keyRecovered = mykey.encode("utf-8")
+    encryptKeyRecovered = Fernet(keyRecovered)
+    # Encrypt password from byte form with str.encode
+    encryptPass = encryptKeyRecovered.encrypt(str.encode(mypassword))
+    # Decode encrypted password to string form to be stored
+    encryptPassDecoded = encryptPass.decode("utf-8")
+    return encryptPassDecoded
+
+# Decrypt password using key
+def passdecrypt(mykey, mypasswordencrypted):
+    # Take key from stored form and convert it back to byte form
+    keyRecovered = mykey.encode("utf-8")
+    encryptKeyRecovered = Fernet(keyRecovered)
+    # Using recovered key unencrypt stored password
+    encryptPassEncode = mypasswordencrypted.encode("utf-8")
+    originalPassByte = encryptKeyRecovered.decrypt(encryptPassEncode)
+    originalPass = originalPassByte.decode("utf-8")
+    return originalPass
 
 # Get command line arguments
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        passencrypt(sys.argv[1])
+    if len(sys.argv) == 2 and sys.argv[1] == "key":
+        yourKey = passkey()
+        print ("Here is your encryption key:", yourKey)
+    elif len(sys.argv) == 4 and sys.argv[1] == "encrypt":
+        yourPassEncrypted = passencrypt(sys.argv[2], sys.argv[3])
+        print ("Here is your encrypted password:", yourPassEncrypted)
+    elif len(sys.argv) == 4 and sys.argv[1] == "decrypt":
+        yourPass = passdecrypt(sys.argv[2], sys.argv[3])
+        print ("Here is your password:", yourPass)
     else:
-        print ("Syntax: " + sys.argv[0] + " [password]")
+        print ("Syntax:")
+        print ("        " + sys.argv[0] + " key")
+        print ("          Run with the word key to recieve a key")
+        print ("        " + sys.argv[0] + " encrypt [key] [password]")
+        print ("          Send a valid key along with a password to receive an encrypted version")
+        print ("        " + sys.argv[0] + " decrypt [key] [password]")
+        print ("          Send a valid key along with an encrypted password to receive the original password")
